d78e61cb308e7bdac9016b7ef03cff7008bc8787
Matteo Tarda <t_matteo@ymail.com>
2019-02-04 23:59:05 +0100

    Finished assignment.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,44 @@
+let options;
+
+const mappa = new Mappa('Leaflet');
+let myMap;
+
+let canvas;
+
+var locationData;
+
 function preload(){
-  // put preload code here
+    locationData =  getCurrentPosition();
 }
 
 function setup() {
-  // put setup code here
+    canvas = createCanvas(windowWidth,windowHeight);
+
+    // Create a tile map and overlay the canvas on top.
+    options = {
+        lat: locationData.latitude,
+        lng: locationData.longitude,
+        zoom: 4,
+        style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    }
+    myMap = mappa.tileMap(options);
+    myMap.overlay(canvas);
+
+    myMap.onChange(drawOnMap);
+
+    fill('#fff5'); 
+    stroke('#000');
 }
 
 function draw() {
-  // put drawing code here
+
+}
+
+function drawOnMap() {
+    // Clear the canvas
+    clear();
+
+    const pos = myMap.latLngToPixel(locationData.latitude,locationData.longitude);
+    ellipse(pos.x,pos.y,20);
+
 }
